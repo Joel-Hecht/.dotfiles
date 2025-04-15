34afaabbd472f0e77e7f5cdcb1a05bfe752af046
Joel Hecht <156119383+Joel-Hecht@users.noreply.github.com>
2025-04-14 23:59:55 -0400

    added a few more to home_fs and added package install script

---
+++ install_pkgs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install \
+i3-wm `necessary for i3`\
+i3lock \
+i3blocks \
+suckless-tools \
+fonts-jetbrains-mono `fonts`\
+fonts-font-awesome \
+fonts-powerline \
+konsole `my preferred terminal emulator`\
+compton `allows transparent terminals` \
+maim `needed for screenshots` \
+xclip \
+xdotool \
+nitrogen `desktop background manager` \
+wget sed grep `cant believe this isnt default`\
+gcc make cmake vim neovim  `general tools` \
+fdisk `volume viwer thats worst than lsblk but i like it`\
+
+
+
