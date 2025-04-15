fa718af2a1d280840cf7f62b9816c023ca64af4d
Joel Hecht <156119383+Joel-Hecht@users.noreply.github.com>
2025-04-14 23:01:47 -0400

    initial commit w/ creator script

---
+++ makesymlinks.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# for recursive calls, get the current dir that we 
+# are working on
+if [ -z "$1" ]; then
+	dir=$(echo $0 | sed 's/[^\/]*$//')
+	dir=$(realpath "$dir")
+	prefix=$dir
+else
+	dir=$1
+	prefix=$2
+fi
+
+#enable * top check hidden files
+shopt -s dotglob
+
+for i in $dir*; do
+	if [ -d "$i" ]; then
+		if [[ "$i" != "." ]] && [[ "$i" != ".." ]]; then
+			echo "directory found: $i"
+			$0 "$i/" "$prefix"
+		fi
+	elif [ -f "$i" ]; then
+		r=$( echo $i | sed -e "s/${prefix//\//\\/}//" )
+		
+		current="$2$r"
+		projected="$HOME$r"
+		
+		if [ "$current" == $(realpath $0) ]; then
+			#we do not want to make symlink of this bash file
+			continue
+		fi
+		
+		echo "creating symlink $current -> $projected"
+		
+		if [ -e $projected ]; then
+			echo "removing file"
+			rm $projected
+		fi		
+	
+		$(ln -s "$current" "$projected")
+
+	else
+		echo "no files in $i"
+	fi
+done
+
+#* will no longer include hidden files after exit
+shopt -u dotglob
+
