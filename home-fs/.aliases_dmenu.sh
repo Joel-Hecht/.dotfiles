c364b7e2d3c25362deb0a594a6192a03243f834a
Joel Hecht <156119383+Joel-Hecht@users.noreply.github.com>
2025-05-11 13:53:39 -0400

    flatpak aliases

--- home-fs/.aliases_dmenu.sh
+++ home-fs/.aliases_dmenu.sh
@@ -5,3 +5,5 @@ alias teams="firefox --new-window  https://teams.microsoft.com/v2/ &"
 alias tumblr="firefox --new-window  https://www.tumblr.com &"
 alias fencing="firefox --new-window --profile "$(realpath $HOME/.mozilla/firefox/*\.Drexel\ Fencing | sed -e 's/ /\\ /')" & " #uses profile specific to Joe desktop, dont use
 alias keychron="chromium 'https://launcher.keychron.com' &"
+alias slack="flatpak run com.slack.Slack &"
+alias pinta="flatpak run com.github.PintaProject.Pinta &"
