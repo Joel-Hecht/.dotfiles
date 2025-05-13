6f30df20ac54f06c48d10e391b1bef491224ac44
Joel Hecht <156119383+Joel-Hecht@users.noreply.github.com>
2025-05-13 00:21:59 -0400

    updated policykit and install for gnome

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -22,7 +22,8 @@ gcc make cmake vim neovim  `general tools` \
 fdisk `volume viwer thats worst than lsblk but i like it`\
 git gh `if you got this far you should already have this` \
 libx11-dev `x11 support, needed for multi-monitor config` \
-calcurse `in-terminal calendar` 
+calcurse `in-terminal calendar` \
+policykit-1-gnome polkitd `polkit needed to authenticate as root from i3wm`  
 
 
 #flatpaks
