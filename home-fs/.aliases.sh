2b4b6bb63b342da4f951c14355d19e942ce35746
rasen68 <chriszhao6868@gmail.com>
2025-06-02 02:12:41 -0400

    volume changes and some bs

--- home-fs/.aliases.sh
+++ home-fs/.aliases.sh
@@ -50,3 +50,8 @@ alias ssh="TERM=xterm-256color ssh"
 
 #source aliases that act as applications
 source ${HOME}/.aliases_dmenu.sh
+
+#force load keybinds whenever you do basically anything (i really like rshift = escape)
+alias firefox="i3-msg -q restart && firefox"
+alias vim="i3-msg -q restart && vim"
+alias vi="i3-msg -q restart && vi"
