c0ff5583f18d10886562ceb5c5acc614f65f95c5
Joel Hecht <156119383+Joel-Hecht@users.noreply.github.com>
2026-02-02 17:15:25 +0000

    [fix](nvim) treesitter explicitly told to use outdated master branch

--- home-fs/.config/nvim/init.lua
+++ home-fs/.config/nvim/init.lua
@@ -1055,6 +1055,8 @@ require("lazy").setup({
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		branch = "master",
+		-- require("nvim-treesitter").install(),
 		main = "nvim-treesitter.configs", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
