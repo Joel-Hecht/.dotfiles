60fa00dae6a45dbde12689ce60aefeef91bc8ee4
Joel Hecht <156119383+Joel-Hecht@users.noreply.github.com>
2026-03-31 15:23:50 -0400

    [feat](pope) pope floats

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -47,7 +47,8 @@ cowsay \
 build-essential gdb debhelper ` prob alr have these` \
 bat \
 bfs ` breadth-first _find that we build our bfs on top of ` \
-feh `_lightter weight than eog for pope` \
+feh `_lighter weight than eog for pope` \
+wmctrl `_to target x11 windows by pid` \
 gcc `put this here again so ALL PREVIOUS can have escaped newlines`
 # fonts-noto `_display foreign language characters`\
 #libudev-dev libinput-dev libpugixml-dev libcairo2-dev libx11-dev libxtst-dev libxrandr-dev libxi-dev libglib2.0-dev libgtk-3-dev `for touchegg, prob alr have these` \
