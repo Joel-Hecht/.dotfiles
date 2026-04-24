d4cde169879306ef21bbdeeb4747bbeaee2fb05e
Joel Hecht <156119383+Joel-Hecht@users.noreply.github.com>
2026-04-24 12:01:24 -0400

    [feat](aliases) cmake cli options are way too verbose and i want to type them as little as possible

--- home-fs/.aliases.sh
+++ home-fs/.aliases.sh
@@ -54,6 +54,9 @@ function rust {
 }
 alias dust="rm -rf $HOME/.test-rust"
 alias cgb="cargo build"
+alias cdb="cmake -DCMAKE_BUILD_TYPE=Debug"
+alias cbd="cdb" # as funny as it is I find myself typing this all the time instead so whatever
+alias crel="cmake -DCMAKE_BUILD_TYPE=Release"
 
 #computer control
 alias eep="systemctl suspend"
