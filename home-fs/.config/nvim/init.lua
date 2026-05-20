0d048881658e1cc10a694149d47a821385341d97
Joel Hecht <156119383+Joel-Hecht@users.noreply.github.com>
2026-05-20 19:02:26 -0400

    [chore](nvim) <Esc> will exit terminal mode (was <Esc><Esc>), since it is now impossible to create nested terminals per prev commit

--- home-fs/.config/nvim/init.lua
+++ home-fs/.config/nvim/init.lua
@@ -239,14 +239,15 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- CHOPPING BLOCK - DOESNT WORK FOR KITTY AS OF RIGHT NOW
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+--
+-- UPDATE: because neovim-remote makes it impossible to nest sessions, insteadwe will use a single escape (initial kickstart config had <Esc><Esc>)
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
