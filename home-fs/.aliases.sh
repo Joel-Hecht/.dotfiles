09c761db8b65e0af201445408af7458c349a0123
Joel Hecht <156119383+Joel-Hecht@users.noreply.github.com>
2026-07-31 23:15:38 -0400

    [fix](whitespace) gitwhitespace doesnt fail if there are no files (or files with no contents) in diff

--- home-fs/.aliases.sh
+++ home-fs/.aliases.sh
@@ -184,7 +184,7 @@ alias vix="vi -X" # use if vim is slow due to x11 issues
 alias sexy="cowsay sexy!"
 alias sex="sexy"
 alias whitespace="sed -i 's/\s\+$//' \$(fd -t f -X grep -lI .)"
-alias gitwhitespace="sed -i 's/\s\+$//' \$(git diff HEAD --name-only)"
+alias gitwhitespace="if [[ -n \"\$(git diff HEAD --name-only)\" ]]; then sed -i 's/\s\+$//' \$(git diff HEAD --name-only); fi"
 # fixes pdfs and excel spreadsheets becoming transparent
 alias helpme="killall compton && sleep 1 && setsid /usr/bin/compton > /dev/null 2>&1 &"
 
