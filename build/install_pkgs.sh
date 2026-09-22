42f0cbf1c971f3006019dd20a8c7ae7de283067e
legozx777 <153138339+legozx777@users.noreply.github.com>
2026-09-22 19:50:13 -0400

    Fix (install): remove docker curl -y

--- build/install_pkgs.sh
+++ build/install_pkgs.sh
@@ -150,7 +150,7 @@ fi
 # Add Docker's official GPG key:
 sudo apt update
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc -y
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
